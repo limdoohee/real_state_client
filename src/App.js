b403fdd3d9a916da3884cd4f17393539b0d8754d
limdoohee@gmail.com <limdoohee@gmail.com>
2022-09-27 21:15:16 +0900

    공공데이터 API 호출

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [list, setList] = useState([]);
+  const url = `/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptRent?_wadl
+    &type=xml&LAWD_CD=11110&DEAL_YMD=202208&serviceKey=zcjFU2s%2BmkQXYL2DPSsSYXrqXxSqZuPWT5z0c2nVVxLLYuoJCJEg6DrMTfA8dwWA6Y0QSyYlLPAaTzDvbqtkHw%3D%3D`;
+
+  const fetchApi = async () => {
+    try {
+      axios(url, {
+        method: "GET",
+        mode: "no-cors",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/xml; charset=utf-8",
+        },
+        withCredentials: true,
+        credentials: "same-origin",
+      }).then((res) => {
+        setList(res.data.response.body.items.item);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchApi}>리스트</button>
+      <ul>
+        {list.map((item) => (
+          <div>
+            {item.년}/{item.법정동}/{item.보증금액}
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
