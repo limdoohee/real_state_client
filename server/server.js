edf9f96480682f8670bc41dddac92b2ac8e00494
limdoohee@gmail.com <limdoohee@gmail.com>
2022-10-25 22:38:27 +0900

    DB 연결

--- server/server.js
+++ server/server.js
@@ -6,3 +6,20 @@ app.use("/test", test);
 
 const port = 5000;
 app.listen(port, () => console.log(`${port}`));
+
+// get the client
+const mysql = require("mysql2");
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234", // 비번
+  database: "project",
+});
+
+// simple query
+connection.query("SELECT * FROM member", function (err, results, fields) {
+  console.log(results); // results contains rows returned by server
+  //   console.log(fields); // fields contains extra meta data about results, if available
+});
