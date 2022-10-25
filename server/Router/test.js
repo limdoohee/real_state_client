ceeb8156b55ce1f734cc7bddc9e8d10641473b88
limdoohee@gmail.com <limdoohee@gmail.com>
2022-10-25 23:30:16 +0900

    데이터 조회 및 라우팅 설정

--- server/Router/test.js
+++ server/Router/test.js
@@ -1,8 +1,23 @@
 const express = require("express");
-const router = express.Router();
+const router = express();
+const mysql = require("mysql2");
 
-router.get("/", (req, res) => {
-  res.send({ test: "hi" });
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "project",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+router.get("/test", (req, res) => {
+  pool.query("SELECT * FROM member", function (err, rows, fields) {
+    // Connection is automatically released when query resolves
+    err ? console.log(err) : console.log(rows);
+    res.send(rows);
+  });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,25 +1,8 @@
 const express = require("express");
 const app = express();
-const test = require("./Router/test");
+const api = require("./Router/test");
 
-app.use("/test", test);
+app.use("/api", api);
 
 const port = 5000;
 app.listen(port, () => console.log(`${port}`));
-
-// get the client
-const mysql = require("mysql2");
-
-// create the connection to database
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234", // 비번
-  database: "project",
-});
-
-// simple query
-connection.query("SELECT * FROM member", function (err, results, fields) {
-  console.log(results); // results contains rows returned by server
-  //   console.log(fields); // fields contains extra meta data about results, if available
-});
